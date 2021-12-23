d1becc525a559b5b8b097a8e82fc506f8e00b11f
danil <schubin@qwe.rt>
2021-12-23 15:52:46 +0300

    add LandingPage

--- src/user.js
+++ src/user.js
@@ -1,3 +1,12 @@
 import $ from "jquery";
-
-$('.user').html('User is working');
+import AirDatepicker from "air-datepicker";
+import "air-datepicker/air-datepicker.css";
+new AirDatepicker("#el", {
+  dateFormat(date) {
+    return date.toLocaleString("ja", {
+      year: "numeric",
+      day: "2-digit",
+      month: "long",
+    });
+  },
+});
