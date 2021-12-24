60663d1038e3e394657020668329f0769eaf20ed
danil <schubin@qwe.rt>
2021-12-24 10:08:20 +0300

    bb

--- src/user.js
+++ src/user.js
@@ -1,12 +1,1 @@
 import $ from "jquery";
-import AirDatepicker from "air-datepicker";
-import "air-datepicker/air-datepicker.css";
-new AirDatepicker("#el", {
-  dateFormat(date) {
-    return date.toLocaleString("ja", {
-      year: "numeric",
-      day: "2-digit",
-      month: "long",
-    });
-  },
-});
